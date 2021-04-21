227423d6bb735f2a8a4d755fdb245ee893ee9e86
DHJariwala <65482665+DHJariwala@users.noreply.github.com>
2021-04-21 20:24:57 +0530

    Changed Passwords
    Comments beside the statement is the password

--- Data.sql
+++ Data.sql
@@ -1,5 +1,5 @@
-insert into Admin values('Mukesh','pbkdf2:sha256:150000$3UlX87RR$e36f3df807efdd01b8c795015b92d8fd2c0f373e7b63a3c51e654a59db674f5e');
-insert into Admin values('Ratan','pbkdf2:sha256:150000$HXvW06mk$d5c4a03fb7ab0e10410b2bebf8c03232450fe7ba0b6f13c3d2c22f1697e38663');
+insert into Admin values('Mukesh','pbkdf2:sha256:150000$nG7ZKMgo$aacb97e2d76b07335c2c55ab4b037c39fd825eb3db0635a754b803789aa795b4'); -- adminabcd
+insert into Admin values('Ratan','pbkdf2:sha256:150000$7a4hqmD6$2dfb0d7c228ea6ea6ef44b3749aafb407c4b42911c033d7c8436931962bf45dd'); -- admindef
 
 insert into Person values('1','Mahesh Bhatt','40','Male','1231231231');
 insert into Person values('2','Suresh Bhatt','20','Male','1231231232');
@@ -23,18 +23,18 @@ insert into Person values('19','Vikash Rumalwala','32','Male','8688688681');
 insert into Person values('20','Sneha Agarwal','43','Female','2929299921');
 insert into Person values('21','Shital Sharma','34','Female','1199119923');
 
-insert into House values('101','pbkdf2:sha256:150000$YMSMt0I1$b414cd2c443db48e94e6038133c36dd64e25ddb4bced1b7ec5056ef1f9be2a33','1');
-insert into House values('102','pbkdf2:sha256:150000$YMSMt0I1$b414cd2c443db48e94e6038133c36dd64e25ddb4bced1b7ec5056ef1f9be2a33','6');
-insert into House values('201','pbkdf2:sha256:150000$4cMJuAkU$d6d91d8daa19f916cff581b28eb5c444ed12eac7644f8f7bef5571349747c985','10');
-insert into House values('202','pbkdf2:sha256:150000$B2bWTsE1$6788cb1eb2906ba6e770d0dde78086ae5c0703a5bfda93d0a8e4c46e2e355533','12');
-insert into House values('301','pbkdf2:sha256:150000$LFsUKbsR$1fa44008f89c1fc33352b194ce6c7130507778485f8b35ea1706957cea1ca7ad','14');
-insert into House values('302','pbkdf2:sha256:150000$ZTqCArFD$c40f8ba528aae2eb0969e8b53049d228b80ca33205cc9acbb6fae634511d52fe','16');
+insert into House values('101','pbkdf2:sha256:150000$WaPJ6r88$fb5b174160e71b6976b4a70769b81c1776dfa621a9a616b4320e7b92705a6324','1'); -- mypassword
+insert into House values('102','pbkdf2:sha256:150000$YMSMt0I1$b414cd2c443db48e94e6038133c36dd64e25ddb4bced1b7ec5056ef1f9be2a33','6'); -- password
+insert into House values('201','pbkdf2:sha256:150000$4cMJuAkU$d6d91d8daa19f916cff581b28eb5c444ed12eac7644f8f7bef5571349747c985','10'); -- NextGenGame
+insert into House values('202','pbkdf2:sha256:150000$p6o0d0RG$b2092ffe6a4906a63c132b6b83784201bb094b8f01b62c6ba8f68c6c78ea2a62','12'); -- hithereman
+insert into House values('301','pbkdf2:sha256:150000$LFsUKbsR$1fa44008f89c1fc33352b194ce6c7130507778485f8b35ea1706957cea1ca7ad','14'); -- KittyCat123
+insert into House values('302','pbkdf2:sha256:150000$ZTqCArFD$c40f8ba528aae2eb0969e8b53049d228b80ca33205cc9acbb6fae634511d52fe','16'); -- Incorrect101
 insert into House values('401','pbkdf2:sha256:150000$WLjcbY47$339b2615a1f6cf2c490808623b39a06a738ce02e61824c2417549b941644ff43','20'); -- greatgambit
 
-insert into Staff values('5','pbkdf2:sha256:150000$PWQ1lrWF$197c7c296b6d225ecdb2d3b387f72557a38ea7ce869b15fc854e738907f15820','15000');
-insert into Staff values('18','pbkdf2:sha256:150000$WT9jalhS$5ccea2fe37162cd8d186cf76c8089f74dd20220a2d341fb55ded23cc7e1d83cd','20000');
-insert into Staff values('19','pbkdf2:sha256:150000$jgMBHkU5$5f2c0dafd60f1e35b7c31bfdc6805f7d9d7f15ffdba91c7e21a344f84d9e2d62','15000');
-insert into Staff values('21','pbkdf2:sha256:150000$75xVl03J$47a020613efd66e4d4bf74dc9d4c84b71829b8393e1fe504d58bb0148f41582a','16000'); -- meandyou
+insert into Staff values('5','pbkdf2:sha256:150000$JAt6OCNI$86aea87d1fd5a2fb2638407fa256b86937d03fe69fd5c01fb351f8e08c586717','15000'); -- ramukakaop
+insert into Staff values('18','pbkdf2:sha256:150000$WT9jalhS$5ccea2fe37162cd8d186cf76c8089f74dd20220a2d341fb55ded23cc7e1d83cd','20000'); -- sandesh101
+insert into Staff values('19','pbkdf2:sha256:150000$jX6GO7iH$2ea800a3d07143d1c56570d6217e53295e28bf9790dc3faa5b7d9f2cb084bf14','15000'); -- vikibhai19
+insert into Staff values('21','pbkdf2:sha256:150000$RBQ7wZIl$3c586232f11b54c77f434fa1769a9d740fc71c12c0a23eee7a6b261826836547','16000'); -- meandyoulove
 
 insert into Resident values('1','101');
 insert into Resident values('2','101');
