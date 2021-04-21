164debcde12fc8e31ded61934da8e3e37ec48056
DHJariwala <65482665+DHJariwala@users.noreply.github.com>
2021-04-21 21:27:46 +0530

    Auto increment triggers
    3 auto increment triggers added. Person, Guest, Complaint.

---
+++ trigger.sql
@@ -0,0 +1,29 @@
+create or replace trigger ai_person
+before insert on Person
+for each row
+declare 
+x number;
+begin
+select count(to_number(Person_ID)) into x from Person;
+:new.Person_ID := x + 1;
+end;
+
+create or replace trigger ai_guest
+before insert on Guest
+for each row
+declare
+x number;
+begin
+select count(to_number(Guest_ID)) into x from Guest;
+:new.Guest_ID := x + 1;
+end;
+
+create or replace trigger ai_complaint
+before insert on Complaint
+for each row
+declare
+x number;
+begin
+select count(to_number(Complaint_ID)) into x from Complaint;
+:new.Complaint_ID := x + 1;
+end;
