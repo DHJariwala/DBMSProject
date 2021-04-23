a3bda3cfc42e8013fa59abccd6e07230e7c75b89
DHJariwala <65482665+DHJariwala@users.noreply.github.com>
2021-04-24 00:00:11 +0530

    dob, views and data
    function left due to errors

--- views.sql
+++ views.sql
@@ -2,11 +2,7 @@
 -- Complaint table + Owner name + Staff name
 -- Reason: To give owner name and staff name along with complaint details
 -- Delete the current view by 'drop materialized view complaint_view;'
-create materialized view complaint_view 
-build immediate
-refresh complete
-on demand
-as
+create view complaint_view as
 select t1.Complaint_ID,Status,C_TimeStamp,Description,House_No,Owner_name,Staff_ID,Staff_name from
 (select Complaint_ID,Status,C_TimeStamp,Description,Complaint.House_No,Person.Name as Owner_name 
 from Complaint
@@ -46,18 +42,10 @@ on t1.Complaint_ID = t2.Complaint_ID;
 -- Route: /staff/search
 -- Residents info with house no
 -- Reason: Can be used for search without Joining resident with person always
-create materialized view Resident_search_view 
-build immediate
-refresh complete
-on demand
-as
+create view Resident_search_view as
 select house_no, person_id, phone_no from ((select person_id, name, phone_no from person) join (select house_no, resident_id from resident) on person_id = resident_id);
 
 -- Route: 
 -- Residnets info from Person, discarding Staff
-create materialized view Preson_without_Staff 
-build immediate
-refresh complete
-on demand
-as
+create view Preson_without_Staff as
 select * from Person where Person_ID not in (select Staff_ID from Staff);
