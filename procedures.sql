aede38df8fbbf799f2c347cbd483efbf280ed79e
DHJariwala <devjariwala76@gmail.com>
2021-04-25 12:35:16 +0530

    updated procedures

--- procedures.sql
+++ procedures.sql
@@ -1,21 +1,21 @@
 create or replace procedure insert_person_staff
-(nam in varchar2,ag in number, 
+(nam in varchar2,ag in date, 
 gen in varchar2,phno in numeric,pass in varchar2,sal in int)
 is
     ids number;
 begin
-    insert into Person values('1',nam,ag,gen,phno);
+    insert into Person (Person_ID,Name,DOB,Gender,Phone_No) values('1',nam,ag,gen,phno);
     select max(to_number(Person_ID)) into ids from Person;
     insert into Staff values(ids,pass,sal);
 end insert_person_staff;
 
 create or replace procedure insert_person_resident
-(nam in varchar2,ag in number, 
+(nam in varchar2,ag in date, 
 gen in varchar2,phno in numeric,hno in varchar2)
 is
     ids number;
 begin
-    insert into Person values('1',nam,ag,gen,phno);
+    insert into Person (Person_ID,Name,DOB,Gender,Phone_No) values('1',nam,ag,gen,phno);
     select max(to_number(Person_ID)) into ids from Person;
     insert into Resident values(ids,hno);
 end insert_person_resident;
